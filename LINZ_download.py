f25f9cd5e3aefdccf43b676340939b71a8017a79
elizabeth-fw <ewil195@aucklanduni.ac.nz>
2024-01-24 12:18:17 +1300

    eodag setup

--- LINZ_download.py
+++ LINZ_download.py
@@ -2,3 +2,38 @@
 # has search functionality for date and geometry
 # use eodag pystac together?
 
+from eodag import EODataAccessGateway
+from eodag.plugins.search.qssearch import QueryStringSearch
+
+# Set the STAC catalog URL
+catalog_url = "https://nz-imagery.s3-ap-southeast-2.amazonaws.com/catalog.json"
+
+# Create an EODataAccessGateway instance
+dag = EODataAccessGateway()
+
+# Define the area of interest (bounding box coordinates in [min_lon, min_lat, max_lon, max_lat] format)
+min_lon, min_lat, max_lon, max_lat = 175.2, -36, 175.6, -36.4
+
+# Set the search parameters
+LINZ_search_parameters = {
+    "geom": {"lonmin": 175.2, "latmin": -36, "lonmax": 175.6, "latmax": -36.4}
+}
+
+# Search for available products using the STAC catalog
+search_results = dag.search(QueryStringSearch(**LINZ_search_parameters), catalog_url)
+
+# Display information about the search results
+if search_results:
+    print("Found matching products:")
+    for product in search_results:
+        print(f"- Product ID: {product.properties['id']}")
+        print(f"  Date: {product.properties['datetime']}")
+        print(f"  Cloud Cover: {product.properties['eo:cloud_cover']}%")
+        print(f"  Platform: {product.properties['platform']}")
+        print(f"  Instrument: {product.properties['instrument']}")
+        print(f"  Thumbnail URL: {product.properties['thumbnail']}")
+        print("")
+
+else:
+    print("No products found matching the search criteria.")
+
--- S2_download.py
+++ S2_download.py
@@ -1,37 +1,64 @@
-from eodag import setup_logging
-setup_logging(3)
 
+# Peps has half data for some years (ex. 2023) and shows errors for others (ex. 2021)
+# ONDA does not have S2 data for aotea
+# sentinelsat is not recognized anymore as the previous website was shut down in Oct 2023
+# 2017 search results - 44 on creodias
+# 2018 search results - 46 on creodias
+# 2019 search results - 92 on creodias
+# 2020 search results - 92 on creodias
+# 2021 search results - 96 on creodias
+# 2022 search results - 50 on creodias
+# 2023 search results - 44 on creodias
+
+################ SEARCH #######################
 from eodag import EODataAccessGateway
 dag = EODataAccessGateway()
-dag.available_providers("S2_MSI_L1C")
-
-[product_type["ID"] for product_type in dag.list_product_types("onda")]
-
-
-search_criteria = {
-    "productType": "S2_MSI_L1C",
-    "start": "2023-11-01",
-    "end": "2023-12-31",
-    "geom": {"lonmin": 175.2, "latmin": -36, "lonmax": 175.6, "latmax": -36.4}
-}
-products_first_page, estimated_total_number = dag.search(**search_criteria)
-print(f"Got {len(products_first_page)} products and an estimated total number of {estimated_total_number} products.")
-
-
-# Use  eodag code with peps to download S2 data
-# ONDA does not have S2 data for aotea
-# try using sentinelsat
-
-
-dag = EODataAccessGateway()
-#dag.set_preferred_provider("peps")
+dag.set_preferred_provider("creodias")
 
 # Carry out the search and download
 search_criteria = {
     "productType": "S2_MSI_L1C",
-    "start": "2021-11-01",
-    "end": "2021-12-31",
+    "start": "2017-11-01",
+    "end": "2017-12-31",
     "geom": {"lonmin": 175.2, "latmin": -36, "lonmax": 175.6, "latmax": -36.4}
 }
 
 products = dag.search_all(**search_criteria)
+
+
+
+############### DOWNLOAD #######################
+import os
+from eodag import EODataAccessGateway
+from eodag import setup_logging
+setup_logging(3)
+
+#Setup user credentials and directory
+dag = EODataAccessGateway()
+dag.set_preferred_provider("creodias")
+dag.providers_config["creodias"].auth.credentials["totp"] = "854632"
+
+save_dir = "C:/Users/ewil195/Documents/S2"
+if not os.path.isdir(save_dir):
+    os.mkdir(save_dir)
+
+# Carry out the search and download
+search_criteria = {
+    "productType": "S2_MSI_L1C",
+    "start": "2022-11-01",
+    "end": "2022-12-31",
+    "geom": {"lonmin": 175.2, "latmin": -36, "lonmax": 175.6, "latmax": -36.4}
+}
+
+products = dag.search_all(**search_criteria)
+
+if not products:
+    print("No products found matching the search criteria.")
+else:
+    for product in products:
+        print('Trying to download:', product.properties['title'])
+        try:
+            dag.download(product, progress_callback=None, wait=2, timeout=20, destination_uri=save_dir)
+            print(f"Downloaded {product.properties['title']} to {save_dir}")
+        except Exception as e:
+            print(f"Error downloading {product.properties['title']}: {str(e)}")
