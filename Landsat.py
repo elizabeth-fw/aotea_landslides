5be2da693741ac8c7c36ed0ab3282d96f19b2be2
elizabeth-fw <ewil195@aucklanduni.ac.nz>
2024-02-20 18:07:33 +1300

    Looping function to process all tar files for landsat

--- Landsat.py
+++ Landsat.py
@@ -30,14 +30,14 @@ def extract_tar(tar_file, extract_folder):
     with tarfile.open(tar_file, "r") as tar:
         file_names_in_tar = tar.getnames()
 
-    # Check if each file already exists in teh destination folder
+    # Check if each file already exists in the destination folder
     files_to_extract = []
     for i in file_names_in_tar:
         dest_path = os.path.join(extract_folder, i)
         if not os.path.exists(dest_path):
             files_to_extract.append(i)
 
-    # Extract the files that do not alreayd exist in the destination folder
+    # Extract the files that do not already exist in the destination folder
     if files_to_extract:
         with tarfile.open(tar_file, "r") as tar:
             tar.extractall(path=extract_folder,
@@ -67,22 +67,42 @@ def clip_raster_aoi(raster_file, aoi_geom, output_file):
     print("Clipped raster saved to:", output_file)
 
 
-def clip_tar_raster(tar_file, extract_folder, aoi_geom):
+
+def clip_tar_raster(tar_file, extract_folder, aoi_geom, output_folder):
     extract_tar(tar_file, extract_folder)
 
     band_paths = glob.glob(os.path.join(extract_folder, "*.TIF"))
     for j in band_paths:
-        new_fpath = j.split(".")[0]+"_rproj.TIF"
+        new_fpath = os.path.join(output_folder, os.path.basename(j).split(".")[0] + "_rproj.TIF")
         reproject_raster(j, new_fpath)
         os.remove(j)
 
     band_paths = glob.glob(os.path.join(extract_folder, "*_rproj.TIF"))
     for i in band_paths:
-        new_fpath = i.split(".")[0]+"_clipped.TIF"
+        new_fpath = os.path.join(output_folder, os.path.basename(i).split(".")[0] + "_clipped.TIF")
         clip_raster_aoi(i, aoi_geom, new_fpath)
         os.remove(i)
 
 
+def process_all_tar_files(base_dir, img_drv_dir, aoi_geom):
+    year_subfolders = [d for d in os.listdir(os.path.join(base_dir, "Landsat")) if os.path.isdir(os.path.join(base_dir, "Landsat", d))]
+
+    for year_folder in year_subfolders:
+        year_folder_path = os.path.join(base_dir, "Landsat", year_folder)
+
+        # Create a corresponding output folder for the processed TIFs
+        output_folder = os.path.join(img_drv_dir, "landsat_ard", year_folder)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        # Loop through all tar files in the current year subfolder
+        tar_files = [f for f in os.listdir(year_folder_path) if f.endswith(".tar")]
+        for tar_file in tar_files:
+            tar_file_path = os.path.join(year_folder_path, tar_file)
+            tmp_dir = os.path.join(base_dir, "tmp_dir")
+
+            # Process the tar file and save the TIFs in the corresponding output folder
+            clip_tar_raster(tar_file_path, tmp_dir, aoi_geom, output_folder)
 
 
 
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-sys.path.append("C:/Users/tdow214/OneDrive - The University of Auckland/Documents/GitHub/aotea_landslides")
-from Landsat import read_shapefile, clip_tar_raster
+sys.path.append("C:/Users/ewil195/PycharmProjects/aotea_landslides")
+from Landsat import read_shapefile, clip_tar_raster, process_all_tar_files
 
 # Directories
 img_base_dir = "Z:/Raw_data/Aotea/"
-vector_base_dir = "C:/Users/tdow214/OneDrive - The University of Auckland/Documents/Projects/Landslides/"
+vector_base_dir = "C:/Users/ewil195/PycharmProjects/aotea_landslides/"
 img_drv_dir = "Z:/Derived_data/Aotea_landslides/"
 tmp_dir = os.path.join(img_base_dir, "tmp_dir")
 if not os.path.exists(tmp_dir):
@@ -15,11 +15,11 @@ if not os.path.exists(tmp_dir):
 
 # Import AOI
 aoi_path = os.path.join(vector_base_dir,
-                        "Aotea_AOI.shp")
+                        "aotea_shp/aotea.shp")
 aotea_aoi = read_shapefile(aoi_path)
 
-
-# Import Landsat Data
+"""
+# Import and Clip Landsat Data
 landsat_ard_dir = os.path.join(img_drv_dir, "landsat_ard")
 if not os.path.exists(landsat_ard_dir):
     os.mkdir(landsat_ard_dir)
@@ -28,15 +28,29 @@ tar_file = os.path.join(img_base_dir,
                         "Landsat/2023/LC08_L1TP_073085_20231101_20231109_02_T1.tar")
 
 clip_tar_raster(tar_file, tmp_dir, aotea_aoi)
+"""
+
+# Process all TAR files
+#landsat_dir = os.path.join(img_drv_dir, "Landsat")
+
+process_all_tar_files(img_base_dir, img_drv_dir, aotea_aoi)
 
 
 # Functions - Apply Scaling Factors
 
+## could clip & scale in one function
+## add another subfolder of each image within year
 
 # Functions - Cloud Masking
 
 
+# Export TIF with scaling & cloudmasking in one
+
+
 # Functions - Visualizations
 
+## optimize for me seeing landslides
+## only have three channels - how to work with bands in those
+## read on what is the best combination for viewing landslides - landslide optimized false color image
 
 # Data Processing and Composite Creation
